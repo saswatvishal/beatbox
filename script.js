25eb05b1f725cdda2225aea9a987cd9dfaef7815
saswatvishal <taptosaswat@gmail.com>
2021-06-30 11:36:59 +0530

    first commit

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 
 let beats = {
     "65": {
-        beat: new Beat("./assets/Piano Chord 331.mp3"),
+        beat: new Beat("./audio/Piano Chord 331.mp3"),
         button: new Button("rgb(200, 255, 0)", 65)
     },
     "83": {
-        beat: new Beat("./assets/Piano Chord 209.mp3"),
+        beat: new Beat("./audio/Piano Chord 209.mp3"),
         button: new Button("rgb(200, 255, 0)", 83)
     },
     "68": {
-        beat: new Beat("./assets/Piano Chord 208.mp3"),
+        beat: new Beat("./audio/Piano Chord 208.mp3"),
         button: new Button("rgb(200, 255, 0)", 68)
     },
     "70": {
-        beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
+        beat: new Beat("./audio/Drum Sticks Hit 3.mp3"),
         button: new Button("rgb(0, 255, 179)", 70)
     },
     "71": {
-        beat: new Beat("./assets/Drum Snare Roll.mp3"),
+        beat: new Beat("./audio/Drum Snare Roll.mp3"),
         button: new Button("rgb(0, 255, 179)", 71)
     },
     "72": {
-        beat: new Beat("./assets/PREL Musical 57.mp3"),
+        beat: new Beat("./audio/PREL Musical 57.mp3"),
         button: new Button("rgb(0, 255, 179)", 72)
     },
     "74": {
-        beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
+        beat: new Beat("./audio/Cymbal Suspended 2.mp3"),
         button: new Button("rgb(255, 0, 55)", 74)
     },
     "75": {
-        beat: new Beat("./assets/Musical Compos 33.mp3"),
+        beat: new Beat("./audio/Musical Compos 33.mp3"),
         button: new Button("rgb(255, 0, 55)", 75)
     },
     "76": {
-        beat: new Beat("./assets/Musical Orches 4.mp3"),
+        beat: new Beat("./audio/Musical Orches 4.mp3"),
         button: new Button("rgb(255, 0, 55)", 76)
     }
 }
